cb6b53a770b5f3f701bcaabb474df9a258697320
Jeffrey <matheusunitt@gmail.com>
2024-01-04 07:15:23 -0300

    Remove uso do context api

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-import { useState } from "react";
-import { TodosContext } from "./Contexts/TodosContext";
-
 import "./styles/global.css";
 
 import Header from "./components/Header";
@@ -8,22 +5,12 @@ import Box from "./components/Box";
 import Footer from "./components/Footer";
 
 const App = () => {
-  const [todos, setTodos] = useState([
-    { text: "Estudar desenvolvimento web", completed: false, id: 0 },
-    { text: "Estudar francês", completed: false, id: 1 },
-    { text: "Treinar guitarra", completed: true, id: 2 },
-    { text: "Ir para a academia", completed: true, id: 3 },
-    { text: "Meditar", completed: false, id: 4 },
-  ]);
-
   return (
-    <TodosContext.Provider value={{ todos, setTodos }}>
-      <div className="container flex flex-col gap-10 mx-auto md:w-[40rem]">
-        <Header />
-        <Box />
-        <Footer />
-      </div>
-    </TodosContext.Provider>
+    <div className="container flex flex-col gap-10 mx-auto md:w-[40rem]">
+      <Header />
+      <Box />
+      <Footer />
+    </div>
   );
 };
 
